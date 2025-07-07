225a647c348e770ea9971f1f12d406728a5b7ad0
Jeffrey Serio <hyperreal@moonshadow.dev>
2025-07-07 11:04:27 -0500

    Add atop

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,19 +3,21 @@
 set -ouex pipefail
 
 # Install packages
-dnf5 install -y borgmatic btrfs-assistant firefox nmap node-exporter yakuake
+dnf5 install -y atop borgmatic btrfs-assistant firefox nmap node-exporter yakuake
 
 # Install VSCodium
 VSCODIUM_RPM_URL=$(curl --silent "https://api.github.com/repos/VSCodium/vscodium/releases/latest" | grep "browser_download_url" | grep "x86_64.rpm" | cut -d : -f 2,3 | tr -d '" ' | head -n 1)
 dnf5 install -y $VSCODIUM_RPM_URL
 
 # Enable services
+systemctl enable atop.service
+systemctl enable atopacct.service
+systemctl enable atop-rotate.timer
 systemctl enable podman.socket
 systemctl enable prometheus-node-exporter.service
 
 # Disable services
 systemctl disable bluetooth.service
-systemctl disable bolt.service
 systemctl disable cups.service
 systemctl disable zfs-mount.service
 systemctl disable zfs-share.service
